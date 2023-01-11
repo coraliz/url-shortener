df4391afd739f085d501312f97ac9d1bc861f94f
Your Name <you@example.com>
2023-01-11 11:34:15 +0200

    restore test file

--- url_shortener/tests.py
+++ url_shortener/tests.py
@@ -15,26 +15,26 @@ class UrlShortenerTestCase(TransactionTestCase):
     def test_short_url_randomness(self):
         self.assertNotEqual(self.djangoproject.short_url, self.stackoverflow.short_url)
 
-    # def test_invalid_url_exception(self):
-    #     try:
-    #         Shortener.objects.create(url="wwwwwww")
-    #         self.fail('Succeeded in creating an object when it received an invalid long url')
-    #     except ValidationError:
-    #         pass
+    def test_invalid_url_exception(self):
+        try:
+            Shortener.objects.create(url="wwwwwww")
+            self.fail('Succeeded in creating an object when it received an invalid long url')
+        except ValidationError:
+            pass
 
-    # def test_invalid_short_url_characters_exception(self):
-    #     try:
-    #         Shortener.objects.create(url="https://www.google.com/", short_url="14--")
-    #         self.fail('Succeeded in creating an object when it received an invalid short url characters')
-    #     except ValidationError:
-    #         pass
+    def test_invalid_short_url_characters_exception(self):
+        try:
+            Shortener.objects.create(url="https://www.google.com/", short_url="14--")
+            self.fail('Succeeded in creating an object when it received an invalid short url characters')
+        except ValidationError:
+            pass
 
-    # def test_invalid_short_url_length_exception(self):
-    #     try:
-    #         Shortener.objects.create(url="https://www.google.com/", short_url="ABDcccccccc")
-    #         self.fail('Succeeded in creating an object when it received an invalid short url length')
-    #     except ValidationError:
-    #         pass
+    def test_invalid_short_url_length_exception(self):
+        try:
+            Shortener.objects.create(url="https://www.google.com/", short_url="ABDcccccccc")
+            self.fail('Succeeded in creating an object when it received an invalid short url length')
+        except ValidationError:
+            pass
 
     def test_invalid_short_url_unique(self):
         google = Shortener.objects.create(url="https://www.google.com",
@@ -45,7 +45,6 @@ class UrlShortenerTestCase(TransactionTestCase):
         s = 'http://127.0.0.1:8000/s/' + self.djangoproject.short_url
         response = self.client.get(s)
         self.assertRedirects(response, self.djangoproject.url, status_code=302, fetch_redirect_response=False)
-        # self.assertRedirects(response, self.djangoproject.url, status_code=301, fetch_redirect_response=False)
 
     def test_short_url_failure_redirection(self):
         response = self.client.get('http://127.0.0.1:8000/s/1122')
